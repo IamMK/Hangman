75d3fff248577b890cd0d7fd1ebc34bd2363dd5f
IamMK <malk@int.pl>
2018-12-31 06:10:56 +0100

    Structure of files and first data of project

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hangman">
+    <Word></Word>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header"
+import Word from "./components/Word"
+import Footer from "./components/Footer"
+
+export default {
+  name: 'app',
+  components:{
+    Word
+  },
+  data () {
+    return {
+      tries: 5,
+      lose: false,
+      win: false
+    }
+  },
+  watch: {
+    tries: function(){
+      if(tries <= 0){
+        this.lose = true;
+        console.log("You sicky dick :)");
+      }
+    },
+  },
+  methods: {
+    fail: function(){
+      this.tries--;
+    }
+  }
+}
+</script>
---
+++ src/components/Word.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="main__display__word">
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Word",
+    data(){
+        return {
+            word: ["H", "a", "n", "g", "m", "a", "n", " ", "t", "h", "e", " ", "g", "a", "m", "e"],
+        }
+
+    }
+}
+</script>
